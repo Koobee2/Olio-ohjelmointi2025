19dcbbea615781571201e148803741aa6ffc05dc
Koobee2 <kbergbac24@students.oamk.fi>
2025-01-07 12:21:16 +0200

    vk1t1

---
+++ vk1t1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//int game(){
+int game(int maxnum) {
+    srand(time(0));
+
+    int arvottuluku = rand() % maxnum + 1;
+    int arvaus;
+    int arvauslaskuri = 0;
+
+    while (arvaus != arvottuluku) {
+        std::cout << "Arvaa luku 1-" << maxnum << ": ";
+        std::cin >> arvaus;
+        arvauslaskuri++;
+
+        if (arvaus < arvottuluku) {
+            std::cout << "Luku on suurempi" << std::endl;
+        }
+        else if (arvaus > arvottuluku) {
+            std::cout << "Luku on pienempi" << std::endl;
+        }
+        else {
+            std::cout << "Luku on oikein!" << std::endl;
+        }
+    }
+
+    return arvauslaskuri;
+}
+int main(){
+    int maxnum;
+    std::cout << "Syota suurin luku: ";
+    std::cin >> maxnum;
+
+    int arvauslaskuri = game(maxnum);
+    std::cout << "Arvasit oikein " << arvauslaskuri <<  "  kertaa arvattu" << std::endl;
+
+    return 0;
+}
